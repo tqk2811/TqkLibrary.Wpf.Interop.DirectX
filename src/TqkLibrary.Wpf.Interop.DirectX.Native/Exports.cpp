2deb59b24c30b1300a383990d283a3579155a626
tqk2811 <tqk2811@gmail.com>
2022-11-03 20:22:40 +0700

    remove pch, dllmain

--- src/TqkLibrary.Wpf.Interop.DirectX.Native/Exports.cpp
+++ src/TqkLibrary.Wpf.Interop.DirectX.Native/Exports.cpp
@@ -1,11 +1,20 @@
-#include "pch.h"
 #include "Exports.h"
+
+HRESULT PRINT_FAILED(HRESULT hr) {
+#if _DEBUG
+	_com_error err(hr);
+	OutputDebugString(err.ErrorMessage());
+	OutputDebugString(L"\r");
+#endif
+	return hr;
+}
 
 #define WIDTH 640
 #define HEIGHT 480
 
 REFIID                  surfaceIDDXGI = __uuidof(IDXGISurface);
 REFIID                  surfaceID9 = __uuidof(IDirect3DTexture9);
+
 
 IUnknown* ReleaseInterface(IUnknown* pointer) {
 	if (NULL != pointer) {
@@ -19,7 +28,7 @@ HRESULT InitD3D10(SurfaceQueueInterop& surfaceQueueInterop)
 {
 	HRESULT hr;
 	UINT		DeviceFlags = D3D10_CREATE_DEVICE_BGRA_SUPPORT;
-	//DWORD		dwShaderFlags = D3D10_SHADER_ENABLE_STRICTNESS;
+	DWORD		dwShaderFlags = D3D10_SHADER_ENABLE_STRICTNESS;
 #ifdef _DEBUG
 	// To debug DirectX, uncomment the following lines:
 
@@ -27,12 +36,17 @@ HRESULT InitD3D10(SurfaceQueueInterop& surfaceQueueInterop)
 	//dwShaderFlags	|= D3D10_SHADER_DEBUG;
 #endif
 
-	ID3D10Device1** ppD3D10Device = &surfaceQueueInterop.m_D3D10Device;
-	if (FAILED(hr = D3D10CreateDevice1(NULL, D3D10_DRIVER_TYPE_HARDWARE, NULL,
-		DeviceFlags, D3D10_FEATURE_LEVEL_10_0, D3D10_1_SDK_VERSION, ppD3D10Device)))
-	{
-		return hr;
-	}
+	hr = D3D10CreateDevice1(
+		NULL,
+		D3D10_DRIVER_TYPE_HARDWARE,
+		NULL,
+		DeviceFlags,
+		D3D10_FEATURE_LEVEL_10_0,
+		D3D10_1_SDK_VERSION,
+		&surfaceQueueInterop.m_D3D10Device);
+
+	if (FAILED(hr))
+		return PRINT_FAILED(hr);
 
 	D3D10_VIEWPORT vp;
 	vp.Width = WIDTH;
@@ -50,8 +64,8 @@ HRESULT InitD3D9(SurfaceQueueInterop& surfaceQueueInterop)
 {
 	HRESULT hr;
 
-	IDirect3D9Ex** ppD3D9 = &surfaceQueueInterop.m_pD3D9;
-	Direct3DCreate9Ex(D3D_SDK_VERSION, ppD3D9);
+	hr = Direct3DCreate9Ex(D3D_SDK_VERSION, &surfaceQueueInterop.m_pD3D9);
+	if (FAILED(hr)) PRINT_FAILED(hr);
 
 	if (!surfaceQueueInterop.m_pD3D9)
 	{
@@ -66,8 +80,6 @@ HRESULT InitD3D9(SurfaceQueueInterop& surfaceQueueInterop)
 	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
 
 	{
-		IDirect3DDevice9Ex** ppD3D9Device = &surfaceQueueInterop.m_pD3D9Device;
-
 		hr = surfaceQueueInterop.m_pD3D9->CreateDeviceEx(
 			D3DADAPTER_DEFAULT,
 			D3DDEVTYPE_HAL,
@@ -75,9 +87,9 @@ HRESULT InitD3D9(SurfaceQueueInterop& surfaceQueueInterop)
 			D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED | D3DCREATE_FPU_PRESERVE,
 			&d3dpp,
 			NULL,
-			ppD3D9Device);
+			&surfaceQueueInterop.m_pD3D9Device);
 	}
-
+	if (FAILED(hr)) PRINT_FAILED(hr);
 	return hr;
 }
 
@@ -106,49 +118,38 @@ HRESULT InitSurfaces(SurfaceQueueInterop& surfaceQueueInterop)
 
 	if (!surfaceQueueInterop.m_areSurfacesInitialized)
 	{
-		{
-			ISurfaceQueue** ppABQueue = &surfaceQueueInterop.m_ABQueue;
-			IFC(CreateSurfaceQueue(&desc, surfaceQueueInterop.m_pD3D9Device, ppABQueue));
-		}
+		IFC(CreateSurfaceQueue(&desc, surfaceQueueInterop.m_pD3D9Device, &surfaceQueueInterop.m_ABQueue));
 
 		// Clone the queue
-		{
-			ISurfaceQueue** ppBAQueue = &surfaceQueueInterop.m_BAQueue;
-			IFC(surfaceQueueInterop.m_ABQueue->Clone(&CloneDesc, ppBAQueue));
-		}
+		IFC(surfaceQueueInterop.m_ABQueue
+			->Clone(&CloneDesc, &surfaceQueueInterop.m_BAQueue));
 
 		// Setup queue management
-		{
-			ISurfaceProducer** ppm_BAProducer = &surfaceQueueInterop.m_BAProducer;
-			IFC(surfaceQueueInterop.m_BAQueue->OpenProducer(surfaceQueueInterop.m_D3D10Device, ppm_BAProducer));
-		}
+		IFC(surfaceQueueInterop.m_BAQueue
+			->OpenProducer(surfaceQueueInterop.m_D3D10Device, &surfaceQueueInterop.m_BAProducer));
 
-		{
-			ISurfaceConsumer** ppm_ABConsumer = &surfaceQueueInterop.m_ABConsumer;
-			IFC(surfaceQueueInterop.m_ABQueue->OpenConsumer(surfaceQueueInterop.m_D3D10Device, ppm_ABConsumer));
-		}
+		IFC(surfaceQueueInterop.m_ABQueue
+			->OpenConsumer(surfaceQueueInterop.m_D3D10Device, &surfaceQueueInterop.m_ABConsumer));
 
-		{
-			ISurfaceProducer** ppm_ABProducer = &surfaceQueueInterop.m_ABProducer;
-			IFC(surfaceQueueInterop.m_ABQueue->OpenProducer(surfaceQueueInterop.m_pD3D9Device, ppm_ABProducer));
-		}
+		IFC(surfaceQueueInterop.m_ABQueue
+			->OpenProducer(surfaceQueueInterop.m_pD3D9Device, &surfaceQueueInterop.m_ABProducer));
 
-		{
-			ISurfaceConsumer** ppm_BAConsumer = &surfaceQueueInterop.m_BAConsumer;
-			IFC(surfaceQueueInterop.m_BAQueue->OpenConsumer(surfaceQueueInterop.m_pD3D9Device, ppm_BAConsumer));
-		}
+		IFC(surfaceQueueInterop.m_BAQueue
+			->OpenConsumer(surfaceQueueInterop.m_pD3D9Device, &surfaceQueueInterop.m_BAConsumer));
 
 		surfaceQueueInterop.m_areSurfacesInitialized = true;
 	}
 
 Cleanup:
-
+	if (FAILED(hr)) PRINT_FAILED(hr);
 	return hr;
 }
 
 HRESULT _IDirect3DDevice9Ex_CheckDeviceState(IDirect3DDevice9Ex* pD3D9Device)
 {
-	return pD3D9Device->CheckDeviceState(NULL);
+	HRESULT hr = pD3D9Device->CheckDeviceState(NULL);
+	if (FAILED(hr)) PRINT_FAILED(hr);
+	return hr;
 }
 
 HRESULT QueueHelper_GetDXGISurface(SurfaceQueueInterop& surfaceQueueInterop, QueueHelperStruct& queueHelperStruct)
@@ -157,15 +158,24 @@ HRESULT QueueHelper_GetDXGISurface(SurfaceQueueInterop& surfaceQueueInterop, Que
 	UINT size = sizeof(int);
 	DXGI_SURFACE_DESC desc;
 	// Flush the AB queue
-	surfaceQueueInterop.m_ABProducer->Flush(0 /* wait */, NULL);
-	// Dequeue from AB queue
-	hr = surfaceQueueInterop.m_ABConsumer->Dequeue(surfaceIDDXGI, &queueHelperStruct.pUnkDXGISurface, &queueHelperStruct.count, &size, INFINITE);
-	if (FAILED(hr)) return hr;
+	hr = surfaceQueueInterop.m_ABProducer->Flush(0 /* wait */, NULL);
+	//if (FAILED(hr)) 
+	//	PRINT_FAILED(hr);//don't care failed
 
-	hr = queueHelperStruct.pUnkDXGISurface->QueryInterface(surfaceIDDXGI, (void**)&queueHelperStruct.pDXGISurface);
-	if (FAILED(hr)) return hr;
+	// Dequeue from AB queue
+	hr = surfaceQueueInterop.m_ABConsumer
+		->Dequeue(surfaceIDDXGI, &queueHelperStruct.pUnkDXGISurface, &queueHelperStruct.count, &size, INFINITE);
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
+
+	hr = queueHelperStruct.pUnkDXGISurface
+		->QueryInterface(surfaceIDDXGI, (void**)&queueHelperStruct.pDXGISurface);
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
 
 	hr = queueHelperStruct.pDXGISurface->GetDesc(&desc);
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
 
 	return hr;
 }
@@ -176,21 +186,27 @@ HRESULT QueueHelper_GetSurface9(SurfaceQueueInterop& surfaceQueueInterop, QueueH
 
 	// Produce the surface
 	hr = surfaceQueueInterop.m_BAProducer->Enqueue(queueHelperStruct.pDXGISurface, NULL, NULL, SURFACE_QUEUE_FLAG_DO_NOT_WAIT);
-	if (FAILED(hr)) return hr;
+	//if (FAILED(hr)) 
+	//	PRINT_FAILED(hr);//don't care failed
 
 	// Flush the BA queue
 	hr = surfaceQueueInterop.m_BAProducer->Flush(0 /* wait, *not* SURFACE_QUEUE_FLAG_DO_NOT_WAIT*/, NULL);
-	if (FAILED(hr)) return hr;
+	//if (FAILED(hr)) 
+	//	PRINT_FAILED(hr);//don't care failed
 
 	// Dequeue from BA queue
 	hr = surfaceQueueInterop.m_BAConsumer->Dequeue(surfaceID9, &queueHelperStruct.pUnkTexture9, NULL, NULL, INFINITE);
-	if (FAILED(hr)) return hr;
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
 
 	hr = queueHelperStruct.pUnkTexture9->QueryInterface(surfaceID9, (void**)&queueHelperStruct.pTexture9);
-	if (FAILED(hr)) return hr;
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
 
 	// Get the top level surface from the texture
 	hr = queueHelperStruct.pTexture9->GetSurfaceLevel(0, &queueHelperStruct.pSurface9);
+	if (FAILED(hr)) 
+		return PRINT_FAILED(hr);
 
 	return hr;
 }
@@ -199,12 +215,15 @@ HRESULT QueueHelper_ABProducerEnqueueTexture9(SurfaceQueueInterop& surfaceQueueI
 	HRESULT hr = S_OK;
 	// Produce Surface
 	hr = surfaceQueueInterop.m_ABProducer->Enqueue(queueHelperStruct.pTexture9, &queueHelperStruct.count, sizeof(int), SURFACE_QUEUE_FLAG_DO_NOT_WAIT);
-	if (FAILED(hr)) return hr;
+	//if (FAILED(hr)) 
+	//	PRINT_FAILED(hr);//don't care failed
 
 	// Flush the AB queue - use "do not wait" here, we'll block at the top of the *next* call if we need to
 	hr = surfaceQueueInterop.m_ABProducer->Flush(SURFACE_QUEUE_FLAG_DO_NOT_WAIT, NULL);
+	//if (FAILED(hr)) 
+	//	PRINT_FAILED(hr);//don't care failed
 
-	return hr;
+	return S_OK;
 }
 void QueueHelper_Release(QueueHelperStruct& queueHelperStruct)
 {
--- src/TqkLibrary.Wpf.Interop.DirectX.Native/Exports.h
+++ src/TqkLibrary.Wpf.Interop.DirectX.Native/Exports.h
@@ -1,6 +1,43 @@
 #ifndef TQKLIBRARYWPFINTEROPDIRECTXNATIVE_Exports_H
 #define TQKLIBRARYWPFINTEROPDIRECTXNATIVE_Exports_H
 
+#ifdef TQKLIBRARYWPFINTEROPDIRECTXNATIVE_EXPORTS
+#define TQKLIBRARYWPFINTEROPDIRECTXNATIVEEXPORT extern "C" __declspec( dllexport )
+#else
+#define TQKLIBRARYWPFINTEROPDIRECTXNATIVEEXPORT extern "C" __declspec( dllimport )
+#endif
+
+
+#define WIN32_LEAN_AND_MEAN// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <comdef.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <stdio.h>
+
+#if _DEBUG
+#define D3D_DEBUG_INFO
+#endif
+
+#if DIRECTX_SDK
+#include <d3dx9.h>
+#else
+#include "d3d9.h"
+#endif
+
+#include <D3D10_1.h>
+
+#define IFC(x) { hr = (x); if (FAILED(hr)) { goto Cleanup; }}
+#define _ReleaseInterface(x) { if (NULL != x) { x->Release(); x = NULL; }}
+
+#include "SurfaceQueue.h"
+
+
+#include "SurfaceQueueInterop.h"
+#include "QueueHelperStruct.h"
+
+
 TQKLIBRARYWPFINTEROPDIRECTXNATIVEEXPORT HRESULT InitD3D10(SurfaceQueueInterop& surfaceQueueInterop);
 
 TQKLIBRARYWPFINTEROPDIRECTXNATIVEEXPORT HRESULT InitD3D9(SurfaceQueueInterop& surfaceQueueInterop);
